262fbfe8541dae8619a8d18a444cc3a6a34b0ae5
Daeho <Daeho@daehoui-MacBookPro.local>
2020-12-27 18:16:50 +0900

    complete usePreventLeave hooks

---
+++ usePreventLeave/index.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect, useRef } from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  const { enablePrevent, disablePrevent } = usePreventLeave();
+  return (
+    <div className="App">
+      <button onClick={enablePrevent}>Protect</button>
+      <button onClick={disablePrevent}>Unprotect</button>
+    </div>
+  );
+};
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
