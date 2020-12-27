3ef79a2613aec1ed670a076b28b468f561da2313
Daeho <Daeho@daehoui-MacBookPro.local>
2020-12-27 20:02:46 +0900

    complete useNetwork hooks

---
+++ useNetwork/index.js
@@ -0,0 +1,17 @@
+import React, { useState, useEffect, useRef } from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  const handleNetworkChange = (online) => {
+    console.log(online ? "We just went online" : "We are offline");
+  };
+  const onLine = useNetwork(handleNetworkChange);
+  return (
+    <div className="App">
+      <h1>{onLine ? "Online" : "Offline"}</h1>
+    </div>
+  );
+};
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
