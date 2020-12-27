a5f01277763371e255506fc7ccd39997065c14a4
Daeho <Daeho@daehoui-MacBookPro.local>
2020-12-27 21:24:05 +0900

    complete useAxios hooks

---
+++ useAxios/index.js
@@ -0,0 +1,23 @@
+import React, { useState, useEffect, useRef } from "react";
+import ReactDOM from "react-dom";
+import useAxios from "./useAxios";
+
+const App = () => {
+  const { loading, data, error, refetch } = useAxios({
+    url:
+      "https://cors-anywhere.herokuapp.com/https://yts.am/api/v2/list_movies.json"
+  });
+  console.log(
+    `Loading: ${loading}\nError:${error}\nData:${JSON.stringify(data)}`
+  );
+  return (
+    <div className="App">
+      <h1>{data && data.status}</h1>
+      <h2>{loading && "Loading"}</h2>
+      <button onClick={refetch}>Refetch</button>
+    </div>
+  );
+};
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
