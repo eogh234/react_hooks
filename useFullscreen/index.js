e0ea43c73dbbebeea3db1ddcd0cf1e63fe82da30
Daeho <Daeho@daehoui-MacBookPro.local>
2020-12-27 20:47:18 +0900

    complete useFullscreen hooks

---
+++ useFullscreen/index.js
@@ -0,0 +1,21 @@
+import React, { useState, useEffect, useRef } from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  const onFullS = (isFull) => {
+    console.log(isFull ? "We are full" : "We are small");
+  };
+  const { element, triggerFull, exitFull } = useFullscreen(onFullS);
+  return (
+    <div className="App" style={{ height: "1000vh" }}>
+      <div ref={element}>
+        <img src="https://miro.medium.com/max/1400/1*MI686k5sDQrISBM6L8pf5A.jpeg" />
+        <button onClick={exitFull}>Exit fullscreen</button>
+      </div>
+      <button onClick={triggerFull}>Make fullscreen</button>
+    </div>
+  );
+};
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
